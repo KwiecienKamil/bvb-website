a2238ddd1a0b035bb3af7e3203f0fd7838dfeb1e
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2023-07-22 16:23:56 +0200

    finished

--- client/src/Modal/SignInModal.js
+++ client/src/Modal/SignInModal.js
@@ -1,47 +1,67 @@
-import React, { useState } from 'react'
-import  ReactDOM  from 'react-dom'
-import '../css/loginModal.css'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "../css/loginModal.css";
 import axios from "axios";
 
-
 const SignInModal = (props) => {
-  const [emailReg, setEmailReg] = useState('')
-  const [passwordReg, setPasswordReg] = useState('')
-  
+  const [emailReg, setEmailReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+  const [canSignIn, setCanSignIn] = useState(false)
+
   const register = () => {
-    axios.post('http://localhost:5000/register', {
-    email: emailReg,
-    password: passwordReg,
-  }).then((response) => {
-    console.log(response)
-    refreshPage()
-  })
+    if(emailReg.length > 0 && passwordReg > 0) {
+      axios
+      .post("http://localhost:5000/register", {
+        email: emailReg,
+        password: passwordReg,    
+      })
+      .then((response) => {
+        console.log(response)
+      });
+      setEmailReg('')
+      setPasswordReg('')
+      window.location.reload();
+  }else {
+    alert("Email/password can't be empty!")
   }
-
-  const refreshPage = () => {
-    window.location.reload();
-  }
-  return (
-    ReactDOM.createPortal(
-    <div className='modal-container'>
-        <div className="username-container">
-        <label htmlFor="email">Email</label>
-        <input type="email" onChange={(e) => {setEmailReg(e.target.value)}}/>
-        </div>
-        <div className="password-container">
-        <label htmlFor="password">Password</label>
-        <input type="password" onChange={(e) => {setPasswordReg(e.target.value)}}/>
-        </div>
-        <div className="modal-button">
-            <button type='submit' className='sign-in' onClick={register}>SIGN IN</button>
-        </div>
-        <div className="question">
-        <p>Already signed in?<a onClick={props.onShowLogin}>Login</a></p>
-        </div>
-    </div>,
-    document.getElementById('portal')
-  )
-  )
 }
+  
 
-export default SignInModal
+  return ReactDOM.createPortal(
+    <div className="modal-container">
+      <div className="username-container">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          onChange={(e) => {
+            setEmailReg(e.target.value);
+          }}
+          value={emailReg}
+        />
+      </div>
+      <div className="password-container">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          onChange={(e) => {
+            setPasswordReg(e.target.value);
+          }}
+          value={passwordReg}
+        />
+      </div>
+      <div className="modal-button">
+        <button type="submit" className="sign-in" onClick={register}>
+          SIGN IN
+        </button>
+      </div>
+      <div className="question">
+        <p>
+          Already signed in?<a onClick={props.onShowLogin}>Login</a>
+        </p>
+      </div>
+    </div>,
+    document.getElementById("portal")
+  );
+};
+
+export default SignInModal;
